dd19f944d6d730bc376208d0a1e964d470bc5c3d
Andreas Bueff <acbueff@gmail.com>
2015-10-16 13:43:39 -0700

    got 4.2, didn't use graph though...DFS may be questionable

--- CrackingCode/src/treeGraphs/MySolution4_2.java
+++ CrackingCode/src/treeGraphs/MySolution4_2.java
@@ -6,22 +6,49 @@ import linkedLists.Node;
 
 public class MySolution4_2 {
 	
-	public static boolean search(DiGraph graph, Node s, Node d){
+	public static boolean search(Node s, Node d){
 				
-				Queue<Node> search = new LinkedList<>();
+				//Queue<Node> search = new LinkedList<>();
 				
-				if(s == d){return true;}
-				
-				while(s != null){
-					for(Node n: )
-					
+				if(s.getData() == d.getData()){return true;}
+				if(s.getVisited() || s.getAdjacent().isEmpty()){
+					return false;
 				}
+				
+				for(Node a : s.getAdjacent()){
+					if(search( a, d)){
+						return true;
+					}
+				}
+				
+				
 		
 		return false;
 	}
 	
 	
 	public static void main(String[] args){
+		//DiGraph graph = new DiGraph(6);
+		//Node S = (Ngraph.getVertex().getClass();
+		Node S = new Node(1);
+		Node a = new Node(2);
+		Node b = new Node(3);
+		Node c = new Node(4);
+		Node d = new Node(5);
+		Node E = new Node(6);
+		S.addEdge(a);
+		S.addEdge(b);
+		a.addEdge(b);
+		b.addEdge(c);
+		c.addEdge(d);
+		c.addEdge(E);
+		
+		if(search(S,E)){
+			System.out.println("It found a path");
+		}
+		else{
+			System.out.println("No path found");
+		}
 		
 	}
 
